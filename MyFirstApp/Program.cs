a98f46110bd5910e39a9306aa3e223b5706077f4
Andrestktk <andres.altmets@tktk.ee>
2023-04-29 23:02:46 +0300

    first commit

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿//This is acomment to my code
+
+//Rakendus küsib kasutajal sisestada nime
+//Seejärel tervitab rakendus kasutajat nimepidi
+
+Console.WriteLine("Enter you name");  //output
+//string - sõne
+string userName = Console.ReadLine();  //input
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //output
+
